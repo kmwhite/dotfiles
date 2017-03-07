cc4f44d589c44e18dbe65fc9f1758632b817501c
Kristofer M White <me@kmwhite.net>
2017-03-06 18:06:42 -0600

    [shell] Switching to fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+source ~/.asdf/asdf.fish
+
+fundle plugin 'oh-my-fish/plugin-android-sdk'
+fundle plugin 'oh-my-fish/plugin-bang-bang'
+fundle plugin 'oh-my-fish/plugin-tmux'
+fundle plugin 'oh-my-fish/theme-batman'
+fundle plugin 'oh-my-fish/plugin-basename-compat'
+
+fundle init
