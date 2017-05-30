1a9a3a82023b435e6d038127f37403cf275e9db5
Kristofer M White <me@kmwhite.net>
2017-05-30 12:11:15 -0500

    [shell] Remove legacy bash cruft, tmux plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ source ~/.asdf/asdf.fish
 
 fundle plugin 'oh-my-fish/plugin-android-sdk'
 fundle plugin 'oh-my-fish/plugin-bang-bang'
-fundle plugin 'oh-my-fish/plugin-tmux'
 fundle plugin 'oh-my-fish/theme-batman'
 fundle plugin 'oh-my-fish/plugin-basename-compat'
 
