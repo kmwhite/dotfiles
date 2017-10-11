0826b9b137db214491255081781a195a4a76a498
Kristofer M White <me@kmwhite.net>
2017-10-10 19:36:37 -0500

    [fish|x11] Add Dracula Theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ source ~/.asdf/asdf.fish
 
 fundle plugin 'oh-my-fish/plugin-android-sdk'
 fundle plugin 'oh-my-fish/plugin-bang-bang'
-fundle plugin 'oh-my-fish/theme-batman'
+fundle plugin 'nesl247/fish-theme-dracula'
 fundle plugin 'oh-my-fish/plugin-basename-compat'
 
 fundle init
