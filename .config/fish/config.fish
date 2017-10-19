445ce4b20fa78610ef509d3c38258bd4c6ac3bea
Kristofer M White <me@kmwhite.net>
2017-10-18 20:36:27 -0500

    [shell] Preface prompt with new-line

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,26 @@ fundle plugin 'nesl247/fish-theme-dracula'
 fundle plugin 'oh-my-fish/plugin-basename-compat'
 
 fundle init
+
+function functions_rename
+    set -l old_func $argv[1]
+    set -l new_func $argv[2]
+    if test $old_func = $new_func
+        return
+    end
+
+    functions --copy $old_func $new_func
+    functions --erase $old_func
+
+    set -l saved_func $HOME/.config/fish/functions/$old_func.fish
+    if test -f $saved_func
+        funcsave $new_func
+        rm $saved_func
+    end
+end
+
+functions_rename fish_prompt fish_prompt_orig
+function fish_prompt
+  echo ""
+  fish_prompt_orig
+end
